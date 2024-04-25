b43ac31d9bdc05e20e50a51ad66dd6fa86ddd4c7
0nenezero <tempo9134@naver.com>
2024-04-25 22:46:49 +0900

    :sparkles: 서버 응답 생성

--- 004/server.js
+++ 004/server.js
@@ -2,7 +2,10 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(`Request from client: ${req.url}`);
-  console.log(req);
+  // console.log(req);
+  res.setHeader("Content-Type", "text/plain");
+  res.write("Hello world!");
+  res.end();
 });
 server.listen(3000, () => {
   console.log(`Server running at http://localhost:3000/`);
