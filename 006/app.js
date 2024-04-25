9caa36870859698998fa2d532c1ba7c56866136a
0nenezero <tempo9134@naver.com>
2024-04-25 23:19:26 +0900

    :sparkles: express 라우팅 설정

--- 006/app.js
+++ 006/app.js
@@ -3,8 +3,27 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.status(200);
-  res.send("Hello Node!");
+  res.status(200).send("Hello Node!");
+});
+
+app.get("/contacts", (req, res) => {
+  res.status(200).send("Contacts Page");
+});
+
+app.post("/contacts", (req, res) => {
+  res.status(201).send("Create Contacts");
+});
+
+app.get("/contacts/:id", (req, res) => {
+  res.status(200).send(`View Contacts ${req.params.id}`);
+});
+
+app.put("/contacts/:id", (req, res) => {
+  res.status(200).send(`Update Contacts ${req.params.id}`);
+});
+
+app.delete("/contacts/:id", (req, res) => {
+  res.status(200).send(`Delete Contacts ${req.params.id}`);
 });
 
 app.listen(PORT, () => {
