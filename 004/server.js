f80d2ea28e7875ec613a6bd3edf382093f2ed658
0nenezero <tempo9134@naver.com>
2024-04-25 22:51:03 +0900

    :sparkles: html 문서 응답 처리

--- 004/server.js
+++ 004/server.js
@@ -1,12 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(`Request from client: ${req.url}`);
   // console.log(req);
-  res.setHeader("Content-Type", "text/plain");
-  res.write("Hello world!");
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+  const readStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+  readStream.pipe(res);
 });
+
 server.listen(3000, () => {
   console.log(`Server running at http://localhost:3000/`);
 });
