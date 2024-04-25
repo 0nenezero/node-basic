d67e00f5409beff68e9d35a1a9a97ba47c1730f3
0nenezero <tempo9134@naver.com>
2024-04-25 23:48:49 +0900

    :sparkles: 오류 처리를 위한 미들웨어 추가

--- 007/app.js
+++ 007/app.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = 3000;
 
 const router = express.Router();
+const errorhandler = require("./middleware/errorhandler");
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello Node!");
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/contacts", require("./routes/contactRouter"));
+
+app.use(errorhandler);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
