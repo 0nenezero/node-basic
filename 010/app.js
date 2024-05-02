09b3eb0e25f948d1935c877e22aedc6f34f6568f
0nenezero <tempo9134@naver.com>
2024-05-02 22:30:15 +0900

    :sparkles: 템플릿 엔진에 변수 전달

---
+++ 010/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+const dbConnect = require("./config/dbConnect");
+
+const router = express.Router();
+const errorhandler = require("./middleware/errorhandler");
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Node!");
+});
+
+dbConnect();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/contacts", require("./routes/contactRouter"));
+
+app.use(errorhandler);
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
---
+++ 010/controllers/contactController.js
@@ -0,0 +1,89 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+
+// @desc Get all contacts
+// @route GET /contacts
+
+const getAllContacts = asyncHandler(async (req, res) => {
+  try {
+    const contacts = await Contact.find();
+    const users = [
+      { name: "John", email: "john@aaa.bbb", phone: "123456" },
+      { name: "Jane", email: "jane@aaa.bbb", phone: "987654" },
+    ];
+    res.render("getAllContacts", { heading: "User List", users });
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+// @desc Create a contact
+// @route POST /contacts
+const createContact = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    return res.status(400).send(`A required value has not been entered.`);
+  }
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).send("Create Contacts");
+});
+
+// @desc Get contact
+// @route GET /contacts/:id
+const getContact = asyncHandler(async (req, res) => {
+  // const name = req.params.id;
+  // const contact = await Contact.findOne({ name });
+  const contact = await Contact.findById(req.params.id);
+  res.status(200).send(contact);
+});
+
+// @desc Update contact
+// @route PUT /contacts/:id
+const updateContact = asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const { name, email, phone } = req.body;
+  // const contact = await Contact.findById(id);
+  // if (!contact) {
+  //   res.status(404);
+  //   throw new Error("Contact not found");
+  // }
+
+  // contact.name = name;
+  // contact.email = email;
+  // contact.phone = phone;
+  // contact.save();
+
+  // res.status(200).json(contact);
+
+  const updateContact = await Contact.findByIdAndUpdate(
+    id,
+    { name, email, phone },
+    { new: true }
+  );
+  res.status(200).send(updateContact);
+});
+
+// @desc Delete contact
+// @route DELETE /contacts/:id
+const deleteContact = asyncHandler(async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.deleteOne();
+  res.status(200).send(`Delete Contacts ${contact.name}`);
+});
+
+module.exports = {
+  getAllContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
